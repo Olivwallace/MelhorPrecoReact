b86fb2ba9d1e5bc7e9b183a27afed5483a9703b1
JeronVitor <jersonvitor2016@gmail.com>
2023-05-30 17:17:50 -0300

    mudando função de lugar

--- MelhorPrecoApp/src/main/java/Connection/NotaURL.java
+++ MelhorPrecoApp/src/main/java/Connection/NotaURL.java
@@ -1,7 +1,15 @@
 package Connection;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static Utels.Contraction_Map.getPalavra;
 
 public class NotaURL {
    public String getHtml(String endereco){
@@ -33,5 +41,64 @@ public class NotaURL {
 
       return resp;
    }
-  
+   public static void produtos(String result) {
+      Document html = Jsoup.parse(result);
+      Elements trElements = html.select("#myTable tr");
+      Elements tdElements;
+
+      for (Element trElement : trElements) {
+         tdElements = trElement.select("td");
+         Element td = tdElements.get(0);
+         String tmp = td.toString().replaceAll("\n","");
+         String nome = tmp.substring((tmp.indexOf("<h7>")+6),(tmp.indexOf("</h7>")));
+         nome = nome.replaceAll("\\.", " ");
+         String []vetPalavras = nome.split(" ");
+         int palavras = (ehUnidade(vetPalavras[vetPalavras.length-1]))? vetPalavras.length-1: vetPalavras.length;
+         Object [] nomeProduto = new Object[palavras];
+         for (int i = 0; i < palavras; i++) {
+            ArrayList<String> lista =  getPalavra(vetPalavras[i]);
+            if(lista != null){
+               if(lista.size()==1){
+                  nomeProduto[i] = lista.get(0);
+               } else if(lista.size()>1) {
+                  nomeProduto[i] = lista;
+               }
+            }else {
+               nomeProduto[i] = vetPalavras[i];
+            }
+         }
+         String codigo = tmp.substring((tmp.indexOf(": ")+2),(tmp.indexOf(")")));
+         td = tdElements.get(3);
+         tmp = td.toString().replaceAll("\n","");
+         String valor = tmp.substring(tmp.lastIndexOf("R$")+3,(tmp.indexOf("</td>")));
+         System.out.println(Arrays.toString(nomeProduto) +"\t"+codigo+"\t"+valor);
+      }
+   }
+   public static boolean ehUnidade(String s){
+      boolean hasUn = false;
+      String []unMedida = {"kg","ml","1","2","3","4","5","6","7","8","9","0"};
+      int i = 0;
+      while(i<unMedida.length&&(!hasUn)){
+         if(s.contains(unMedida[i])){
+            hasUn = true;
+         }
+         i++;
+      }
+      return hasUn;
+   }
+
+   public void mercado(String result){
+      int inicio = result.indexOf("<table");
+      int fim = result.indexOf("</table>");
+      String tabela = result.substring(inicio,fim);
+      String nomeMercado = tabela.substring((tabela.indexOf("<b>")+3),(tabela.indexOf("</b>")));
+      String CNPJ = tabela.substring((tabela.indexOf("CNPJ")+5),(tabela.indexOf(" -, ")));
+      String endereco = tabela.substring((tabela.indexOf("italic")+9),(tabela.lastIndexOf("</td>")));
+      String []dadosEnd = endereco.split(", ");
+      String rua = dadosEnd[0];
+      int numero = Integer.parseInt(dadosEnd[1]);
+      String cidade = dadosEnd[3].substring(dadosEnd[3].indexOf("-")+1);
+      System.out.println(nomeMercado+"\t"+CNPJ+"\t"+rua+"\t"+numero+"\t"+cidade);
+   }
+
 }
--- MelhorPrecoApp/src/main/java/Service/NotaService.java
+++ MelhorPrecoApp/src/main/java/Service/NotaService.java
@@ -21,6 +21,8 @@ import java.util.Arrays;
 import java.util.Collection;
 import Connection.NotaURL;
 
+
+
 public class NotaService extends QRcodeService {
 
     public NotaService(){
@@ -57,8 +59,8 @@ public class NotaService extends QRcodeService {
                     part.delete();
                     binaryImg(nomeArquivo);
                    String html =  url.getHtml(qrReader(nomeArquivo));
-                    mercado(html);
-                    produtos(html);
+                    url.mercado(html);
+                    url.produtos(html);
                 }
                 numero++;
             }
@@ -70,65 +72,6 @@ public class NotaService extends QRcodeService {
         multipartConfigElement = null;
         parts = null;
         return "OK";
-    }
-    public static void produtos(String result) {
-        Document html = Jsoup.parse(result);
-        Elements trElements = html.select("#myTable tr");
-        Elements tdElements;
-
-        for (Element trElement : trElements) {
-            tdElements = trElement.select("td");
-            Element td = tdElements.get(0);
-            String tmp = td.toString().replaceAll("\n","");
-            String nome = tmp.substring((tmp.indexOf("<h7>")+6),(tmp.indexOf("</h7>")));
-            nome = nome.replaceAll("\\.", " ");
-            String []vetPalavras = nome.split(" ");
-            int palavras = (ehUnidade(vetPalavras[vetPalavras.length-1]))? vetPalavras.length-1: vetPalavras.length;
-            Object [] nomeProduto = new Object[palavras];
-            for (int i = 0; i < palavras; i++) {
-                ArrayList<String> lista =  getPalavra(vetPalavras[i]);
-                if(lista != null){
-                    if(lista.size()==1){
-                        nomeProduto[i] = lista.get(0);
-                    } else if(lista.size()>1) {
-                        nomeProduto[i] = lista;
-                    }
-                }else {
-                    nomeProduto[i] = vetPalavras[i];
-                }
-            }
-            String codigo = tmp.substring((tmp.indexOf(": ")+2),(tmp.indexOf(")")));
-            td = tdElements.get(3);
-            tmp = td.toString().replaceAll("\n","");
-            String valor = tmp.substring(tmp.lastIndexOf("R$")+3,(tmp.indexOf("</td>")));
-            System.out.println(Arrays.toString(nomeProduto) +"\t"+codigo+"\t"+valor);
-        }
-    }
-    public static boolean ehUnidade(String s){
-        boolean hasUn = false;
-        String []unMedida = {"kg","ml","1","2","3","4","5","6","7","8","9","0"};
-        int i = 0;
-        while(i<unMedida.length&&(!hasUn)){
-            if(s.contains(unMedida[i])){
-                hasUn = true;
-            }
-            i++;
-        }
-        return hasUn;
-    }
-
-    public void mercado(String result){
-        int inicio = result.indexOf("<table");
-        int fim = result.indexOf("</table>");
-        String tabela = result.substring(inicio,fim);
-        String nomeMercado = tabela.substring((tabela.indexOf("<b>")+3),(tabela.indexOf("</b>")));
-        String CNPJ = tabela.substring((tabela.indexOf("CNPJ")+5),(tabela.indexOf(" -, ")));
-        String endereco = tabela.substring((tabela.indexOf("italic")+9),(tabela.lastIndexOf("</td>")));
-        String []dadosEnd = endereco.split(", ");
-        String rua = dadosEnd[0];
-        int numero = Integer.parseInt(dadosEnd[1]);
-        String cidade = dadosEnd[3].substring(dadosEnd[3].indexOf("-")+1);
-        System.out.println(nomeMercado+"\t"+CNPJ+"\t"+rua+"\t"+numero+"\t"+cidade);
     }
 
 }
